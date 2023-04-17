da81b5cf48bb1f5f0b829f2f0949b8c6d0598b21
IvanSvitavskiy <ivan.svitavskiy@mail.ru>
2023-04-17 17:21:10 +0300

    created WebCarApplication

---
+++ WebCar/src/main/java/com/webcar/WebCar/WebCarApplication.java
@@ -0,0 +1,13 @@
+package com.webcar.WebCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCarApplication.class, args);
+	}
+
+}
