8a2fa13e23e59a56b78b8a1ae803599747ca6deb
IvanSvitavskiy <ivan.svitavskiy@mail.ru>
2023-04-17 19:00:55 +0300

    added MainController

---
+++ WebCar/src/test/java/com/webcar/WebCar/WebCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcar.WebCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
